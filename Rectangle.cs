a1a1a599f310cbcba93310d090166ce3efb8b8e9
MKdea <misha212032@gmail.com>
2026-02-25 15:21:58 +0500

    Создание классов фигур

---
+++ Rectangle.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeometryShape
+{
+    public class Rectangle : Shape
+    {
+        private double Width { get; set; }
+        private double Height { get; set; }
+
+        public Rectangle(double width, double height)
+        {
+            if (width <= 0 || height <= 0)
+            {
+                throw new ArgumentException("Входимые данные меньше или равны 0");
+            }
+            
+            Width = width;
+            Height = height;
+        }
+
+        public override double Area()
+        {
+            double result = Width * Height;
+
+            return result;
+        }
+
+        public override double Perimeter()
+        {
+            double result = 2 * (Width + Height);
+
+            return result;
+        }
+
+        public override string ToString()
+        {
+            return $"Прямоугольник: ширина-{Width}, длина-{Height}";
+        }
+    }
+}
---
+++ Triangle.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeometryShape
+{
+    public class Triangle : Shape
+    {
+        private double SideA { get; set; }
+        private double SideB { get; set; }
+        private double SideC { get; set; }
+
+        public Triangle(double sideA, double sideB, double sideC)
+        {
+            if (sideA <= 0 || sideB <= 0 || sideC <= 0)
+            {
+                throw new ArgumentException("Входимые данные меньше или равны 0");
+            }
+
+            if ((sideA + sideB) < sideC || (sideC + sideB) < sideA || (sideA + sideC) < sideB)
+            {
+                throw new Exception("Сумма 2 сторон меньше оставшейся");
+            }
+
+            SideA = sideA;
+            SideB = sideB;
+            SideC = sideC;
+        }
+
+        public override double Area()
+        {
+            double p = (SideA + SideB + SideC) / 2;
+
+            double s = p * (p - SideA) * (p - SideB) * (p - SideC);
+
+            double result = Math.Sqrt(s);
+
+            return result;
+        }
+
+        public override double Perimeter()
+        {
+            double result = SideA + SideB + SideC;
+
+            return result;
+        }
+
+        public override string ToString()
+        {
+            return $"Треугольник: сторона A-{SideA}, сторона B-{SideB}, сторона C-{SideC}";
+        }
+    }
+}
